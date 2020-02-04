02f6cc78f76980ae1d0b5d5797fa27944e64d25f
qianlan <qlhdtly12189@163.com>
2020-02-04 18:45:16 +0800

    carousel views

---
+++ vue/src/views/carousel/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div style="width: 800px; margin: auto">
+    <carousel :items="items" :hide-dir="false" />
+  </div>
+</template>
+
+<script>
+import items from "@components/carousel/items";
+import Carousel from "@components/carousel";
+export default {
+  data() {
+    return {
+      items
+    };
+  },
+  components: {
+    Carousel
+  }
+};
+</script>

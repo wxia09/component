5690142b2095d2fbfbf842f4cd8588cedced8599
qianlan <qlhdtly12189@163.com>
2020-02-04 16:40:01 +0800

    react init

---
+++ react/src/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './App.css';
+import Router from './router';
+export default class APP extends Component {
+    render() {
+        return  (
+            <Router />
+        );
+    }
+};

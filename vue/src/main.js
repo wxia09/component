1d020866f608c36e7c77bedfdb1ee0b6e41832a2
wixa <qlhdtly12189@163.com>
2020-02-05 23:43:29 +0800

    beforeEach

--- vue/src/main.js
+++ vue/src/main.js
@@ -8,6 +8,20 @@ import axios from "./http";
 
 Vue.prototype.$http = axios;
 
+// 全局拦截
+router.beforeEach((to, from, next) => {
+  let token = window.localStorage.getItem("token");
+  if (to.meta.requireAuth) {
+    if (token) {
+      next();
+    } else {
+      // 这里做相应的操作
+    }
+  } else {
+    next();
+  }
+});
+
 new Vue({
   store,
   router,
